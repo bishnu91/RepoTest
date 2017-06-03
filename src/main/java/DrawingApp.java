27503da877853802b4cbe289ca57f728c1af18eb
bishnu91 <aerobishnu@gmail.com>
2017-06-02 20:05:33 -0400

    fisrt checkin

---
+++ src/main/java/DrawingApp.java
@@ -0,0 +1,9 @@
+
+public class DrawingApp {
+public static void main(String[] args){
+	Triangle triangle = new Triangle();
+	triangle.draw();	
+	
+}
+
+}
---
+++ src/main/java/Triangle.java
@@ -0,0 +1,7 @@
+
+public class Triangle {
+
+	public void draw(){
+		System.out.println("I am triangle");
+	}
+}
